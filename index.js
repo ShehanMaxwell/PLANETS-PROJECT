230f2f717a0e0fedf9e7a4898fba07122f1111a0
Shehan Maxwell <shehanbbn@gmail.com>
2023-08-19 12:35:15 +0530

    modified index.js in .

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+// const parse = require('csv-parse');
+// const fs = require('fs');
+
+// const results = [];
+
+// fs.createReadStream('kepler_data.csv')
+//   .pipe(
+//     parse({
+//       comment: '#',
+//       headers: true,
+//     })
+//   )
+//   .on('data', (data) => {
+//     results.push(data);
+//   })
+//   .on('error', (err) => {
+//     console.log(err);
+//   })
+//   .on('end', () => {
+//     console.log(results);
+//     console.log('done');
+//   });
+
 const fs = require('fs');
 const csv = require('csv-parser');
 
